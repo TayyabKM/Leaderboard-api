0f40a70bc2ce956ff6325a30851bd351c9425ace
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-02-01 09:47:56 +0100

    set up dev

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line
-import _ from 'lodash';
-import dataInsert from './modules/dataInput.js';
-import htmlInput from './modules/interaction.js';
-import './style.css';
-
-htmlInput(dataInsert);
